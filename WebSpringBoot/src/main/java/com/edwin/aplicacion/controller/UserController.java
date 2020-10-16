882370884f69ce49e91d642751d2f2b9952230d1
Dell <Dell@DESKTOP-18PL71K>
2020-10-15 23:04:15 -0500

    configuracion de html's

---
+++ WebSpringBoot/src/main/java/com/edwin/aplicacion/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.edwin.aplicacion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/userform")
+	public String userForm() {
+		return "user-form/user-view-form";
+	}
+}
